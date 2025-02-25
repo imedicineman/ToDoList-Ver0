21f209edbaa4351cfda850260f4ab0e8131ff4b7
Robert Delikat <robert.delikat@gmail.com>
2025-02-25 07:44:18 -0800

    Ch. 5.3 Sheets, Lists, and Building the To Do List User Interface is complete and working fine

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -8,30 +8,63 @@
 import SwiftUI
 
 struct DetailView: View {
+    @State var toDo: String
     @Environment(\.dismiss) private var dismiss
-    var passedValue: String
+    @State private var reminderIsOn = false
+    //  @State private var dueDate = Date.now + 60*60*24 will work but added Calendar.current.date.... instead
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+    @State private var notes = ""
+    @State private var isCompleted = false
+    
     var body: some View {
-        VStack {
+        List  {
+            TextField("Enter todo Here", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
             
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You are a Swifty Legend!\nAnd you passed over the value \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            Spacer()
-            //            Button("Back") {
-            //                dismiss()
-            //            }
-            //            .buttonStyle(BorderedProminentButtonStyle())
+            Toggle("Set Reminder", isOn: $reminderIsOn)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            
+            DatePicker("Date:", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .padding(.bottom)
+                .disabled(!reminderIsOn) // only allowed if reminderIsOn
+            
+            Text("Notes:")
+                .padding(.top)
+            
+            TextField("Notes", text: $notes, axis: .vertical) // Axis modifier allows Notes to expand vertically
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            
+            Toggle("Completed", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
         }
-        .padding()
-        //        .navigationBarBackButtonHidden()
+        .listStyle(.plain)
+        .navigationBarBackButtonHidden()
+        
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //TODO: add save code here
+                }
+            }
+        }
     }
 }
-
 // There is a Quirk with "Preview panel: that needs to include what var is  passed. The value does not matter because it will be corrected when the call is made from 'List to detail
 #Preview {
-    DetailView(passedValue: "Item 1")
+    NavigationStack {
+        DetailView(toDo: "")
+    }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -10,26 +10,44 @@ import SwiftUI
 struct ToDoListView: View {
     var toDos = ["Learn Swift",
                  "Build apps",
-                 "Save the world",
-                 "Make a sandwich",
+                 "Save the World",
                  "Take a Vacation"]
-    
+    @State private var sheetIsPresented = false
     var body: some View {
         NavigationStack {
-            //Setup a list of item numbers 0-99 that link to Detail view
+            //Pass elements of an array that link to Detail view
             List {
                 ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
             }
             .navigationBarTitle("To-Do List")
             .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
+            
+            //  .sheet(isPresented: $sheetIsPresented) { uses excess screen real estate instead of card-like overlay at the top of the screen
+            .fullScreenCover(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            // add + sign to upper trailing
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            
         }
-        
     }
 }
 #Preview {
